fa5a1a82a97d72fec1aceeb410ba70be9fa35579
Gert-Jan Anema <Me@gertlily.com>
2014-08-30 17:49:30 +0200

    Push latest version with some changes
    Changelog
    Version 1.** (52) beta
    Added a new init to Xposed (testing purposes)

--- src/com/gertlily/xposed/touchwiz/XposedInit.java
+++ src/com/gertlily/xposed/touchwiz/XposedInit.java
@@ -1,34 +1,34 @@
-package com.gertlily.xposed.touchwiz;
-
-import com.gertlily.xposed.touchwiz.xposed.PhoneStatusBar;
-
-import de.robv.android.xposed.IXposedHookInitPackageResources;
-import de.robv.android.xposed.IXposedHookLoadPackage;
-import de.robv.android.xposed.IXposedHookZygoteInit;
-import de.robv.android.xposed.XSharedPreferences;
-import de.robv.android.xposed.callbacks.XC_InitPackageResources.InitPackageResourcesParam;
-import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
-
-import com.gertlily.xposed.touchwiz.hooks.Common;
-
-public class XposedInit implements IXposedHookZygoteInit, IXposedHookInitPackageResources, IXposedHookLoadPackage {
-	private static XSharedPreferences prefs;
-    
-	@Override
-	public void initZygote(StartupParam startupParam) throws Throwable {
-		prefs = new XSharedPreferences(Common.PACKAGE_NAME);
-		prefs.makeWorldReadable();
-	}
-
-	@Override
-	public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
-		if (lpparam.packageName.equals("com.android.systemui")) {
-			PhoneStatusBar.init(prefs, lpparam.classLoader);
-		}
-	}
-
-	@Override
-	public void handleInitPackageResources(InitPackageResourcesParam resparam) throws Throwable {
-		
-	}
-}
+////package com.gertlily.xposed.touchwiz;
+//
+//import com.gertlily.xposed.touchwiz.xposed.PhoneStatusBar;
+//
+//import de.robv.android.xposed.IXposedHookInitPackageResources;
+//import de.robv.android.xposed.IXposedHookLoadPackage;
+//import de.robv.android.xposed.IXposedHookZygoteInit;
+//import de.robv.android.xposed.XSharedPreferences;
+//import de.robv.android.xposed.callbacks.XC_InitPackageResources.InitPackageResourcesParam;
+//import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+//
+//import com.gertlily.xposed.touchwiz.hooks.Common;
+//
+//public class XposedInit implements IXposedHookZygoteInit, IXposedHookInitPackageResources, IXposedHookLoadPackage {
+//	private static XSharedPreferences prefs;
+//    
+//	@Override
+//	public void initZygote(StartupParam startupParam) throws Throwable {
+//		prefs = new XSharedPreferences(Common.PACKAGE_NAME);
+//		prefs.makeWorldReadable();
+//	}
+//
+//	@Override
+//	public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
+//		if (lpparam.packageName.equals("com.android.systemui")) {
+//			PhoneStatusBar.init(prefs, lpparam.classLoader);
+//		}
+//	}
+//
+//	@Override
+//	public void handleInitPackageResources(InitPackageResourcesParam resparam) throws Throwable {
+//		
+//	}
+//}
--- src/com/gertlily/xposed/touchwiz/xposed/PhoneStatusBar.java
+++ src/com/gertlily/xposed/touchwiz/xposed/PhoneStatusBar.java
@@ -1,37 +1,44 @@
 package com.gertlily.xposed.touchwiz.xposed;
 
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+import android.graphics.Color;
 import android.widget.TextView;
+import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
-import de.robv.android.xposed.XSharedPreferences;
-import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
-public class PhoneStatusBar  {
-    private static final String CLASS_PHONE_STATUSBAR = "com.android.systemui.statusbar.phone.PhoneStatusBar";
-    private static TextView[] mCarrierTextView;
-    
-	public static void init(final XSharedPreferences prefs, final ClassLoader lpparam) {
-		final Class<?> phoneStatusBarClass = XposedHelpers.findClass(CLASS_PHONE_STATUSBAR, lpparam);
-		
-        XposedHelpers.findAndHookMethod(phoneStatusBarClass, "makeStatusBarView", new XC_MethodHook() {
-            @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                Object carrierTextView = XposedHelpers.getObjectField(param.thisObject, "mCarrierLabel");
-                if (carrierTextView instanceof TextView[]) {
-                    if (((TextView[])carrierTextView).length > 0) {
-                        mCarrierTextView = new TextView[] {
-                                (TextView) ((TextView[])carrierTextView)[0]
-                        };
-                    }
-                } else if (carrierTextView instanceof TextView) {
-                    mCarrierTextView = new TextView[] {
-                            (TextView) carrierTextView
-                    };
-                }
-                mCarrierTextView[0].setText("Gertlily");
-            }
-        });
-        
-//        XposedHelpers.findAndHookMethod("com.android.systemui.statusbar.policy.Clock", "updateClock", new XC_MethodHook() {
+public class PhoneStatusBar implements IXposedHookLoadPackage {
+//    private static final String CLASS_PHONE_STATUSBAR = "com.android.systemui.statusbar.phone.PhoneStatusBar";
+//    private static TextView[] mCarrierTextView;
+//    
+//	public static void init(final XSharedPreferences prefs, final ClassLoader lpparam) {
+//		final Class<?> phoneStatusBarClass = XposedHelpers.findClass(CLASS_PHONE_STATUSBAR, lpparam);
+//		
+//        XposedHelpers.findAndHookMethod(phoneStatusBarClass, "makeStatusBarView", new XC_MethodHook() {
+//            @Override
+//            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+//                Object carrierTextView = XposedHelpers.getObjectField(param.thisObject, "mCarrierLabel");
+//                if (carrierTextView instanceof TextView[]) {
+//                    if (((TextView[])carrierTextView).length > 0) {
+//                        mCarrierTextView = new TextView[] {
+//                                (TextView) ((TextView[])carrierTextView)[0]
+//                        };
+//                    }
+//                } else if (carrierTextView instanceof TextView) {
+//                    mCarrierTextView = new TextView[] {
+//                            (TextView) carrierTextView
+//                    };
+//                }
+//                mCarrierTextView[0].setText("Gertlily");
+//            }
+//        });
+//	}
+	
+    public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
+//        if (!lpparam.packageName.equals("com.android.systemui"))
+//            return;
+//
+//        findAndHookMethod("com.android.systemui.statusbar.policy.Clock", lpparam.classLoader, "updateClock", new XC_MethodHook() {
 //            @Override
 //            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
 //                TextView tv = (TextView) param.thisObject;
@@ -40,5 +47,5 @@ public class PhoneStatusBar  {
 //                tv.setTextColor(Color.RED);
 //            }
 //        });
-	}
+    }
 }
